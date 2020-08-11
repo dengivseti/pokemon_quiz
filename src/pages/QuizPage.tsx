1ddfa205eb93d2efbbf203c47ec96984f71046d4
Dmitrii <dengivseti@list.ru>
2020-08-11 12:01:47 +0300

    feat: add props

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,22 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
 import {Question} from "../components/Question"
 import {Grid, Button} from "@material-ui/core"
 import {Answers} from "../components/Answers"
 import {Information} from "../components/Information"
+import {pokemonData} from "../pokemon"
+import {IPokemon} from "../interface"
 
 export const QuizPage: React.FC = () => {
+    const intPokemon = 1
+    const [selectPokemon, setSelectPokemon] = useState<IPokemon | null>()
+    const questionPokemon = pokemonData[intPokemon]
+    const listAnswerPokemons = pokemonData
+
+    const selectedPokemon = (id: number) => {
+        const findPokemon = pokemonData.find(p => p.id === id)
+        if (findPokemon) {
+            setSelectPokemon(findPokemon)
+        }
+    }
+
     return (
         <div>
             <Grid container>
                 <Grid item xs={12}>
-                    <Question/>
+                    <Question pokemon={questionPokemon!}/>
                 </Grid>
                 <Grid container spacing={2}>
                     <Grid item xs={6}>
-                        <Answers/>
+                        <Answers
+                            pokemons={listAnswerPokemons}
+                            goodId={intPokemon}
+                            onSelected={(id: number) => selectedPokemon(id)}
+                        />
                     </Grid>
                     <Grid item xs={6}>
-                        <Information/>
+                        <Information pokemon={selectPokemon!}/>
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
